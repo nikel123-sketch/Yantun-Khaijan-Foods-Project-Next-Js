259c542a91237ded2b14795e342c19d41fe73c55
nikel123-sketch <webdevelopernikel@gmail.com>
2026-03-22 18:13:59 +0800

    foodCard setup

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -1,9 +1,24 @@
+import FoodCard from '@/component/Card/FoodCard';
 import React from 'react';
 
-const FoodsPage = () => {
+const getfoods=async()=>{
+const res = await fetch(
+  "https://taxi-kitchen-api.vercel.app/api/v1/foods/random",
+);
+const data=await res.json();
+return data.foods
+}
+
+const FoodsPage =async () => {
+    const foods=await getfoods();
     return (
         <div>
-            <h1>this is foods page</h1>
+            <h1 className='font-bold text-center text-3xl underline'>Totall Foods ({foods.length})</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
+                {
+                    foods.map(food=><FoodCard key={food.id} food={food}></FoodCard>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/component/Card/FoodCard.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+
+const FoodCard = ({ food }) => {
+  const { id, catId, category, foodImg, price, title } = food;
+
+  return (
+    <div className="bg-white shadow-xl rounded-2xl p-4 border border-gray-200 hover:shadow-2xl transition">
+      {/* Image */}
+      <img
+        src={foodImg}
+        alt={title}
+        className="w-full h-48 object-cover rounded-xl mb-3 border-4 border-green-500"
+      />
+
+      {/* Content */}
+      <div className="space-y-2">
+        <h2 className="text-xl font-bold text-gray-800">{title}</h2>
+
+        <p className="text-sm text-gray-500">
+          Category: <span className="font-semibold">{category}</span>
+        </p>
+
+        <p className="text-lg font-bold text-amber-500">${price}</p>
+      </div>
+
+      {/* Buttons */}
+      <div className="flex gap-2 mt-4">
+        {/* Add to Cart */}
+        <button className="flex-1 bg-green-500 text-white py-1.5 rounded-lg hover:bg-green-600 transition">
+          Add to Cart
+        </button>
+
+        {/* Details */}
+        <Link
+          href={`/foods/${id}`}
+          className="flex-1 text-center bg-blue-500 text-white py-1.5 rounded-lg hover:bg-blue-600 transition"
+        >
+          Details
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default FoodCard;
