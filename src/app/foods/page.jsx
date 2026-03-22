3e4cbbe8864e8700cbf093e67368b6bc5899bc42
nikel123-sketch <webdevelopernikel@gmail.com>
2026-03-22 18:04:19 +0800

    first commit

---
+++ src/app/foods/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FoodsPage = () => {
+    return (
+        <div>
+            <h1>this is foods page</h1>
+        </div>
+    );
+};
+
+export default FoodsPage;
---
+++ src/component/Shared/Navber.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+import NavLink from "../Active/NavLink.jsx";
+
+const Navber = () => {
+  return (
+    <div className="flex justify-between items-center bg-slate-800 font-bold p-2 rounded-xl">
+      <div className="text-2xl text-amber-50">
+        <Link href="/">Foods</Link>
+      </div>
+
+      <div className="flex gap-2">
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/foods">Foods</NavLink>
+        <NavLink href="/reviews">Reviews</NavLink>
+        <NavLink href="/feedback">Feedback</NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Navber;
