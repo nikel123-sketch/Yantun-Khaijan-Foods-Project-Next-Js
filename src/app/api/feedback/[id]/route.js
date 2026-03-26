1bb35af43bf47ebeca09f41daf4b72a647291b78
nikel123-sketch <webdevelopernikel@gmail.com>
2026-03-26 22:35:41 +0700

    others

--- src/app/api/feedback/[id]/route.js
+++
@@ -1,8 +0,0 @@
-import { feedback } from "../../route";
-
-export async function GET(req,{params}){
-    const {id}=await params;
-
-    const singlefeedback= feedback.find(item=>item.id===Number(id))
-    return Response.json(singlefeedback)
-}
